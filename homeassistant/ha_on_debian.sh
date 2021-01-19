807bc99742f0706d51255f1bf80bb2eede1f94ed
Jorge Assunção <j.assuncao@oestedesign.pt>
2021-01-19 14:33:10 +0000

    Adicionado o comando a executar em caso de erro

--- homeassistant/ha_on_debian.sh
+++ homeassistant/ha_on_debian.sh
@@ -2,6 +2,8 @@
 
 # USAGE:
 # sudo bash -c "$(wget -qLO - https://gitlab.jassuncao.work/cpha/install-scripts/-/raw/master/homeassistant/ha_on_debian.sh)"
+#
+# Se der um erro, dar o comando `export PATH=$PATH:/usr/sbin` antes de correr o script novamente
 
 # Configure path
 export PATH=$PATH:/usr/sbin
