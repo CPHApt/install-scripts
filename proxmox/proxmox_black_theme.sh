6c8173214ee854aa076662216a06ea81b6807b45
Jorge Assunção <j.assuncao@oestedesign.pt>
2021-01-21 21:24:56 +0000

    Novo ficheiro

---
+++ proxmox/proxmox_black_theme.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# USAGE:
+# sudo bash -c "$(wget -qLO - https://gitlab.jassuncao.work/cpha/install-scripts/-/raw/master/proxmox/proxmox_black_theme.sh)"
+
+# Download instalation file
+wget https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.py
+
+# Run script
+python3 PVEDiscordDark.py
