8427bf81fffe5e87e350eeca717edc3645e10b12
Jorge Assunção <j.assuncao@oestedesign.pt>
2021-01-21 21:31:04 +0000

    Criação de link para os créditos do script

--- proxmox/proxmox_black_theme.sh
+++ proxmox/proxmox_black_theme.sh
@@ -2,6 +2,9 @@
 
 # USAGE:
 # sudo bash -c "$(wget -qLO - https://gitlab.jassuncao.work/cpha/install-scripts/-/raw/master/proxmox/proxmox_black_theme.sh)"
+#
+# CREDITS:
+# https://github.com/Weilbyte/PVEDiscordDark
 
 # Download instalation file
 wget https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.py
